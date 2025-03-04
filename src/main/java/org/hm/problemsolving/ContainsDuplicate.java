a7b7285b6c0739375cfcba3aab16636f97c20ed9
hemal0105 <hemal0105@gmail.com>
2025-03-05 00:09:23 +0530

    Added comment

--- src/main/java/org/hm/problemsolving/ContainsDuplicate.java
+++ src/main/java/org/hm/problemsolving/ContainsDuplicate.java
@@ -2,6 +2,10 @@ package org.hm.problemsolving;
 
 import java.util.*;
 
+/**
+ * Given an integer array nums, return true if any value appears
+ * at least twice in the array, and return false if every element is distinct.
+ */
 public class ContainsDuplicate {
 
     public static void main(String[] args) {
