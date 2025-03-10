1e0a803123b96b6c77b9f9cc42ed82402dcb7e1f
hemal0105 <hemal0105@gmail.com>
2025-03-10 19:39:19 +0530

    Disappeared Numbers

--- src/main/java/org/hm/problemsolving/DisappearedNumbers.java
+++ src/main/java/org/hm/problemsolving/DisappearedNumbers.java
@@ -3,6 +3,12 @@ package org.hm.problemsolving;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ *
+ * Given an array nums of n integers where nums[i] is in the range [1, n],
+ * return an array of all the integers in the range [1, n] that do not appear in nums.
+ *
+ */
 public class DisappearedNumbers {
 
     public static void main(String[] args) {
