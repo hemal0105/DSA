854a4effb2f69415e89c3513414c976a5d71231a
hemal0105 <hemal0105@gmail.com>
2025-05-01 23:38:32 +0530

    binary tree problems

--- src/main/java/org/hm/problemsolving/BinaryTreeSubTree.java
+++ src/main/java/org/hm/problemsolving/BinaryTreeSubTree.java
@@ -18,13 +18,41 @@ public class BinaryTreeSubTree {
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 
+    private static boolean isSubTreeStr(TreeNode root, TreeNode subRoot) {
+        StringBuilder sbRootTree = new StringBuilder();
+        serialize(root, sbRootTree);
+        String rootStr = sbRootTree.toString();
+
+        StringBuilder sbSubRootTree = new StringBuilder();
+        serialize(subRoot, sbSubRootTree);
+        String subRootStr = sbSubRootTree.toString();
+
+        System.out.println(rootStr);
+        System.out.println(subRootStr);
+        return rootStr.contains(subRootStr);
+    }
+
+    private static void serialize(TreeNode node, StringBuilder strTree) {
+        if (node == null) {
+            strTree.append("#");
+            return;
+        }
+
+        strTree.append("^");
+        strTree.append(node.val);
+        serialize(node.left, strTree);
+        serialize(node.right, strTree);
+    }
+
     public static void main(String[] args) {
         Integer[] p1 = {3, 4, 5, 1, 2};
         Integer[] q1 = {4, 1, 2};
         System.out.println(isSubTree(createTree(p1), createTree(q1)));
+        System.out.println(isSubTreeStr(createTree(p1), createTree(q1)));
 
         Integer[] p2 = {3, 4, 5, 1, 2, null, null, null, null, 0};
         Integer[] q2 = {4, 1, 2};
         System.out.println(isSubTree(createTree(p2), createTree(q2)));
+        System.out.println(isSubTreeStr(createTree(p2), createTree(q2)));
     }
 }
