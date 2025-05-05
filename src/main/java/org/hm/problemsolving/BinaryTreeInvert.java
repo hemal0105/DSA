00dc1187ca2b09feb4e47cfc26702385d1df82aa
hemal0105 <hemal0105@gmail.com>
2025-05-05 23:09:10 +0530

    binary tree problems

--- src/main/java/org/hm/problemsolving/BinaryTreeInvert.java
+++ src/main/java/org/hm/problemsolving/BinaryTreeInvert.java
@@ -17,6 +17,21 @@ public class BinaryTreeInvert {
         return root;
     }
 
+    private static TreeNode invertTreeItr(TreeNode root) {
+        if (root == null) return null;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+            if (node.left != null) queue.add(node.left);
+            if (node.right != null) queue.add(node.right);
+        }
+        return root;
+    }
+
     private static void levelOrderTraversal(TreeNode root) {
         System.out.print("Level Order Traversal: ");
         Queue<TreeNode> queue = new LinkedList<>();
@@ -34,8 +49,10 @@ public class BinaryTreeInvert {
     public static void main(String[] args) {
         Integer[] arr1 = {4, 2, 7, 1, 3, 6, 9};
         levelOrderTraversal(invertTreeRec(createTree(arr1)));
+        levelOrderTraversal(invertTreeItr(createTree(arr1)));
 
         Integer[] arr2 = {2, 1, 3};
         levelOrderTraversal(invertTreeRec(createTree(arr2)));
+        levelOrderTraversal(invertTreeItr(createTree(arr2)));
     }
 }
