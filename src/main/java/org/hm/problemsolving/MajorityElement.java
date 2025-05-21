8529b2be24ecb270fd38200fc04d58a2340a9e5b
hemal0105 <hemal0105@gmail.com>
2025-05-21 22:19:49 +0530

    Majority element

--- src/main/java/org/hm/problemsolving/MajorityElement.java
+++ src/main/java/org/hm/problemsolving/MajorityElement.java
@@ -1,5 +1,6 @@
 package org.hm.problemsolving;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -30,19 +31,59 @@ public class MajorityElement {
         }
 
         int majorityCount = nums.length / 2;
-        for (Map.Entry<Integer, Integer> entry: countMap.entrySet()) {
+        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
             if (entry.getValue() > majorityCount) return entry.getKey();
         }
         return -1;
+    }
+
+    private static int majorityElement3(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+
+    private static int majorityElement4(int[] nums) {
+        return majorityElement4Rec(nums, 0, nums.length - 1);
+    }
+
+    private static int majorityElement4Rec(int[] nums, int low, int high) {
+        if (low == high) {
+            return nums[low];
+        }
+
+        int mid = (high - low) / 2 + low;
+        int left = majorityElement4Rec(nums, low, mid);
+        int right = majorityElement4Rec(nums, mid + 1, high);
+
+        if (left == right) {
+            return left;
+        }
+
+        int leftCount = countInRange(nums, left, low, high);
+        int rightCount = countInRange(nums, right, low, high);
+
+        return leftCount > rightCount ? left : right;
+    }
+
+    private static int countInRange(int[] nums, int num, int low, int high) {
+        int count = 0;
+        for (int i = low; i <= high; i++) {
+            if (nums[i] == num) count++;
+        }
+        return count;
     }
 
     public static void main(String[] args) {
         int[] nums1 = {3, 2, 3};
         System.out.println(majorityElement1(nums1));
         System.out.println(majorityElement2(nums1));
+        System.out.println(majorityElement3(nums1));
+        System.out.println(majorityElement4(nums1));
 
         int[] nums2 = {2, 2, 1, 1, 1, 2, 2};
         System.out.println(majorityElement1(nums2));
         System.out.println(majorityElement2(nums2));
+        System.out.println(majorityElement3(nums2));
+        System.out.println(majorityElement4(nums2));
     }
 }
