bb38b127909061b92896f3034ed0984d01688afe
hemal0105 <hemal0105@gmail.com>
2025-05-21 22:33:31 +0530

    Majority element

--- src/main/java/org/hm/problemsolving/MajorityElement.java
+++ src/main/java/org/hm/problemsolving/MajorityElement.java
@@ -75,14 +75,14 @@ public class MajorityElement {
 
     private static int majorityElement5(int[] nums) {
         int count = 0;
-        int candidate = -1;
+        Integer candidate = null;
 
         for (int num: nums) {
             if (count == 0) candidate = num;
             count += (num == candidate) ? 1 : -1;
         }
 
-        return candidate;
+        return candidate == null ? -1 : candidate;
     }
 
     public static void main(String[] args) {
