6fb65a1cec493c085f3a55da99cf13c5cd545030
hemal0105 <hemal0105@gmail.com>
2025-06-12 12:03:29 +0530

    Is Sub Sequence

--- src/main/java/org/hm/problemsolving/IsSubSequence.java
+++ src/main/java/org/hm/problemsolving/IsSubSequence.java
@@ -7,30 +7,45 @@ public class IsSubSequence {
     private Integer sourceLen;
     private Integer targetLen;
 
-    private boolean isSubsequenceRec(int srcIdx, int tgtIdx) {
+    private boolean isSubsequenceRecUtil(int srcIdx, int tgtIdx) {
         if (srcIdx == sourceLen) return true;
         if (tgtIdx == targetLen) return false;
 
         if (source.charAt(srcIdx) == target.charAt(tgtIdx)) srcIdx++;
         tgtIdx++;
 
-        return isSubsequenceRec(srcIdx, tgtIdx);
+        return isSubsequenceRecUtil(srcIdx, tgtIdx);
     }
 
 
-    private boolean isSubsequence(String source, String target) {
+    private boolean isSubsequenceRec(String source, String target) {
         this.source = source;
         this.target = target;
 
         this.sourceLen = source.length();
         this.targetLen = target.length();
 
-        return isSubsequenceRec(0, 0);
+        return isSubsequenceRecUtil(0, 0);
+    }
+
+    private boolean isSubsequenceItr(String source, String target) {
+        int sourceLen = source.length();
+        int targetLen = target.length();
+
+        int srcIdx = 0, tgtIdx = 0;
+        while (srcIdx < sourceLen && tgtIdx < targetLen) {
+            if (source.charAt(srcIdx) == target.charAt(tgtIdx)) srcIdx++;
+            tgtIdx++;
+        }
+        return srcIdx == sourceLen;
     }
 
     public static void main(String[] args) {
         IsSubSequence isSubSequenceObj = new IsSubSequence();
-        System.out.println(isSubSequenceObj.isSubsequence("abc", "ahbgdc"));
-        System.out.println(isSubSequenceObj.isSubsequence("axc", "ahbgdc"));
+        System.out.println(isSubSequenceObj.isSubsequenceRec("abc", "ahbgdc"));
+        System.out.println(isSubSequenceObj.isSubsequenceRec("axc", "ahbgdcd"));
+
+        System.out.println(isSubSequenceObj.isSubsequenceItr("abc", "ahbgdc"));
+        System.out.println(isSubSequenceObj.isSubsequenceItr("axc", "ahbgdcd"));
     }
 }
