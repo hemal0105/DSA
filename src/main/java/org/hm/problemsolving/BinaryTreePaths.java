11d3f30f2dc1a5a32df23b969927e525fee55d74
hemal0105 <hemal0105@gmail.com>
2025-06-15 22:23:31 +0530

    Binary Tree Paths

--- src/main/java/org/hm/problemsolving/BinaryTreePaths.java
+++ src/main/java/org/hm/problemsolving/BinaryTreePaths.java
@@ -3,6 +3,7 @@ package org.hm.problemsolving;
 import org.hm.util.TreeNode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import static org.hm.problemsolving.BinaryTreeAverageOfLevels.createTree;
@@ -22,17 +23,48 @@ public class BinaryTreePaths {
         }
     }
 
-    private static List<String> binaryTreePaths(TreeNode root) {
+    private static List<String> binaryTreePathsRec(TreeNode root) {
         List<String> paths = new ArrayList<>();
         constructPaths(root, "", paths);
         return paths;
     }
 
+    private static List<String> binaryTreePathsItr(TreeNode root) {
+        List<String> paths = new ArrayList<>();
+        if (root == null) return paths;
+
+        LinkedList<TreeNode> nodeStack = new LinkedList<>();
+        LinkedList<String> pathStack = new LinkedList<>();
+        nodeStack.add(root);
+        pathStack.add(Integer.toString(root.val));
+
+        TreeNode node;
+        String path;
+
+        while (!nodeStack.isEmpty()) {
+            node = nodeStack.pollLast();
+            path = pathStack.pollLast();
+            if (node.left == null && node.right == null) paths.add(path);
+            if (node.left != null) {
+                nodeStack.add(node.left);
+                pathStack.add(path + "->" + node.left.val);
+            }
+            if (node.right != null) {
+                nodeStack.add(node.right);
+                pathStack.add(path + "->" + node.right.val);
+            }
+        }
+
+        return paths;
+    }
+
     public static void main(String[] args) {
         Integer[] arr1 = {1, 2, 3, null, 5};
-        System.out.println(binaryTreePaths(createTree(arr1)));
+        System.out.println(binaryTreePathsRec(createTree(arr1)));
+        System.out.println(binaryTreePathsItr(createTree(arr1)));
 
         Integer[] arr2 = {1};
-        System.out.println(binaryTreePaths(createTree(arr2)));
+        System.out.println(binaryTreePathsRec(createTree(arr2)));
+        System.out.println(binaryTreePathsItr(createTree(arr2)));
     }
 }
