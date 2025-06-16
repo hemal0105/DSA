b24c1c74e7325047f376136c12e967e407b265eb
hemal0105 <hemal0105@gmail.com>
2025-06-16 22:20:14 +0530

    Product of array

---
+++ src/main/java/org/hm/problemsolving/ProductOfArrayExceptSelf.java
@@ -0,0 +1,40 @@
+package org.hm.problemsolving;
+
+import java.util.Arrays;
+
+public class ProductOfArrayExceptSelf {
+
+    // Time - O(n)
+    // Space - O(n)
+    private static int[] productExceptSelf(int[] nums) {
+        int len = nums.length;
+
+        int[] left = new int[len];
+        int[] right = new int[len];
+
+        int[] ans = new int[nums.length];
+
+        left[0] = 1;
+        for (int i = 1; i < len; i++) {
+            left[i] = left[i-1] * nums[i-1];
+        }
+
+        right[len-1] = 1;
+        for (int i = len -2; i >= 0; i--) {
+            right[i] = right[i+1] * nums[i+1];
+        }
+
+        for (int i = 0; i < len; i++) {
+            ans[i] = left[i] * right[i];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 4};
+        System.out.println(Arrays.toString(productExceptSelf(nums1)));
+
+        int[] nums2 = {-1, 1, 0, -3, 3};
+        System.out.println(Arrays.toString(productExceptSelf(nums2)));
+    }
+}
