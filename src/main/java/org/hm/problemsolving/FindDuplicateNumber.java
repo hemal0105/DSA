3cb1bb62b144d75b033401b9fd3299481f449a1c
hemal0105 <hemal0105@gmail.com>
2025-06-18 22:10:49 +0530

    find duplicate

--- src/main/java/org/hm/problemsolving/FindDuplicateNumber.java
+++ src/main/java/org/hm/problemsolving/FindDuplicateNumber.java
@@ -1,6 +1,8 @@
 package org.hm.problemsolving;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class FindDuplicateNumber {
 
@@ -14,14 +16,30 @@ public class FindDuplicateNumber {
         return -1;
     }
 
+    // Not using constant space
+    private static int findDuplicate2(int[] nums) {
+        Set<Integer> uniqueSet = new HashSet<>();
+        for (int num: nums) {
+            if (uniqueSet.contains(num)) return num;
+            uniqueSet.add(num);
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args) {
         int[] nums1 = {1, 3, 4, 2, 2};
         System.out.println(findDuplicate1(nums1));
+        System.out.println(findDuplicate2(nums1));
 
+        System.out.println();
         int[] nums2 = {3, 1, 3, 4, 2};
         System.out.println(findDuplicate1(nums2));
+        System.out.println(findDuplicate2(nums2));
 
+        System.out.println();
         int[] nums3 = {3, 3, 3, 3, 3};
         System.out.println(findDuplicate1(nums3));
+        System.out.println(findDuplicate2(nums3));
     }
 }
