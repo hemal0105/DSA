9181e5d8b59387b1257280d1cf36777f0c33148a
hemal0105 <hemal0105@gmail.com>
2025-06-18 22:24:11 +0530

    find duplicate

--- src/main/java/org/hm/problemsolving/FindDuplicateNumber.java
+++ src/main/java/org/hm/problemsolving/FindDuplicateNumber.java
@@ -27,19 +27,39 @@ public class FindDuplicateNumber {
         return -1;
     }
 
+    // Array modified
+    private static int findDuplicate3(int[] nums) {
+        int duplicate = -1;
+        for (int i = 0; i < nums.length; i++) {
+            int num = Math.abs(nums[i]);
+            if (nums[num] < 0) {
+                duplicate = num;
+                break;
+            }
+            nums[num] *= -1;
+        }
+
+        for (int i = 0; i < nums.length; i++) nums[i] = Math.abs(nums[i]);
+
+        return duplicate;
+    }
+
     public static void main(String[] args) {
         int[] nums1 = {1, 3, 4, 2, 2};
         System.out.println(findDuplicate1(nums1));
         System.out.println(findDuplicate2(nums1));
+        System.out.println(findDuplicate3(nums1));
 
         System.out.println();
         int[] nums2 = {3, 1, 3, 4, 2};
         System.out.println(findDuplicate1(nums2));
         System.out.println(findDuplicate2(nums2));
+        System.out.println(findDuplicate3(nums2));
 
         System.out.println();
         int[] nums3 = {3, 3, 3, 3, 3};
         System.out.println(findDuplicate1(nums3));
         System.out.println(findDuplicate2(nums3));
+        System.out.println(findDuplicate3(nums3));
     }
 }
