ad81a4c586eb7cc9730ac704e884b834d1db16c3
hemal0105 <hemal0105@gmail.com>
2025-06-24 22:12:38 +0530

    find duplicate

--- src/main/java/org/hm/problemsolving/FindDuplicateNumber.java
+++ src/main/java/org/hm/problemsolving/FindDuplicateNumber.java
@@ -102,6 +102,49 @@ public class FindDuplicateNumber {
     }
 
 
+    private static int findMax(int[] nums) {
+        int maxNum = 0;
+        for (int i = 0; i < nums.length; i++) maxNum = Math.max(maxNum, nums[i]);
+        return maxNum;
+    }
+
+    private static int maxBits(int num) {
+        int bits = 0;
+        while(num > 0) {
+            num /= 2;
+            bits++;
+        }
+        return bits;
+    }
+
+    // Bit manipulations with constant space and O(n)
+    private static int findDuplicate7(int[] nums) {
+        int maxNum = findMax(nums);
+        int maxBits = maxBits(maxNum);
+
+        int duplicate = 0;
+        int n = nums.length - 1;
+
+        for (int bit = 0; bit < maxBits; bit++) {
+            int mask = (1 << bit);
+            int baseCount = 0, numsCount = 0;
+
+            for (int i = 0; i <= n; i++) {
+                if ((i & mask) > 0) {
+                    baseCount++;
+                }
+                if ((nums[i] & mask) > 0) {
+                    numsCount++;
+                }
+            }
+            if (numsCount > baseCount) {
+                duplicate |= mask;
+            }
+        }
+        return duplicate;
+    }
+
+
     public static void main(String[] args) {
         int[] nums1 = {1, 3, 4, 2, 2};
         System.out.println(findDuplicate1(nums1));
@@ -110,6 +153,7 @@ public class FindDuplicateNumber {
         System.out.println(findDuplicate4(nums1));
         System.out.println(findDuplicate5(nums1));
         System.out.println(findDuplicate6(nums1));
+        System.out.println(findDuplicate7(nums1));
 
         System.out.println();
         int[] nums2 = {3, 1, 3, 4, 2};
@@ -119,6 +163,7 @@ public class FindDuplicateNumber {
         System.out.println(findDuplicate4(nums2));
         System.out.println(findDuplicate5(nums2));
         System.out.println(findDuplicate6(nums2));
+        System.out.println(findDuplicate7(nums2));
 
         System.out.println();
         int[] nums3 = {3, 3, 3, 3, 3};
@@ -128,5 +173,6 @@ public class FindDuplicateNumber {
         System.out.println(findDuplicate4(nums3));
         System.out.println(findDuplicate5(nums3));
         System.out.println(findDuplicate6(nums3));
+        System.out.println(findDuplicate7(nums3));
     }
 }
