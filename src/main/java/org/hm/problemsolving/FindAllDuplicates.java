baedbda2013f3960e1b2ded0b5d1d84328b9ab7a
hemal0105 <hemal0105@gmail.com>
2025-06-25 23:26:46 +0530

    find all duplicate

--- src/main/java/org/hm/problemsolving/FindAllDuplicates.java
+++ src/main/java/org/hm/problemsolving/FindAllDuplicates.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class FindAllDuplicates {
 
+    // Time - O (n ^ 2)
     private static List<Integer> findDuplicates1(int[] nums) {
         List<Integer> ans = new ArrayList<>();
 
@@ -21,6 +22,7 @@ public class FindAllDuplicates {
         return ans;
     }
 
+    // Time - O (n log n)
     private static List<Integer> findDuplicates2(int[] nums) {
         Arrays.sort(nums);
 
@@ -35,18 +37,37 @@ public class FindAllDuplicates {
         return ans;
     }
 
+    // Time - O (n), Space - O (n)
+    private static List<Integer> findDuplicates3(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        List<Integer> seen = new ArrayList<>();
+
+        for (int num: nums) {
+            if (seen.contains(num)) {
+                ans.add(num);
+            } else {
+                seen.add(num);
+            }
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
         int[] nums1 = {4,3,2,7,8,2,3,1};
         System.out.println(findDuplicates1(nums1));
         System.out.println(findDuplicates2(nums1));
+        System.out.println(findDuplicates3(nums1));
 
         int[] nums2 = {1,1,2};
         System.out.println(findDuplicates1(nums2));
         System.out.println(findDuplicates2(nums2));
+        System.out.println(findDuplicates3(nums2));
 
         int[] nums3 = {1};
         System.out.println(findDuplicates1(nums3));
         System.out.println(findDuplicates2(nums3));
+        System.out.println(findDuplicates3(nums3));
 
 
     }
