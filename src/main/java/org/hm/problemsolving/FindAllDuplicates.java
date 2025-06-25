09718c6ff450a872b400038a2a18f6163e8bf8f6
hemal0105 <hemal0105@gmail.com>
2025-06-25 23:21:21 +0530

    find all duplicate

--- src/main/java/org/hm/problemsolving/FindAllDuplicates.java
+++ src/main/java/org/hm/problemsolving/FindAllDuplicates.java
@@ -1,6 +1,7 @@
 package org.hm.problemsolving;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FindAllDuplicates {
@@ -20,15 +21,32 @@ public class FindAllDuplicates {
         return ans;
     }
 
+    private static List<Integer> findDuplicates2(int[] nums) {
+        Arrays.sort(nums);
+
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                ans.add(nums[i]);
+                i++;
+            }
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
         int[] nums1 = {4,3,2,7,8,2,3,1};
         System.out.println(findDuplicates1(nums1));
+        System.out.println(findDuplicates2(nums1));
 
         int[] nums2 = {1,1,2};
         System.out.println(findDuplicates1(nums2));
+        System.out.println(findDuplicates2(nums2));
 
         int[] nums3 = {1};
         System.out.println(findDuplicates1(nums3));
+        System.out.println(findDuplicates2(nums3));
 
 
     }
