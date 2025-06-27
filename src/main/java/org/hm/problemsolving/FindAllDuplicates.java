453672148d17d7ea61bfc3e82221dcc28118f079
hemal0105 <hemal0105@gmail.com>
2025-06-27 11:19:18 +0530

    find all duplicate

--- src/main/java/org/hm/problemsolving/FindAllDuplicates.java
+++ src/main/java/org/hm/problemsolving/FindAllDuplicates.java
@@ -7,11 +7,13 @@ import java.util.List;
 public class FindAllDuplicates {
 
     // Time - O (n ^ 2)
-    private static List<Integer> findDuplicates1(int[] nums) {
+    private static List<Integer> findDuplicates1(int[] numsOg) {
+        int[] nums = Arrays.copyOf(numsOg, numsOg.length);
+
         List<Integer> ans = new ArrayList<>();
 
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i+1; j < nums.length; j++) {
+            for (int j = i + 1; j < nums.length; j++) {
                 if (nums[j] == nums[i]) {
                     ans.add(nums[i]);
                     break;
@@ -23,7 +25,9 @@ public class FindAllDuplicates {
     }
 
     // Time - O (n log n)
-    private static List<Integer> findDuplicates2(int[] nums) {
+    private static List<Integer> findDuplicates2(int[] numsOg) {
+        int[] nums = Arrays.copyOf(numsOg, numsOg.length);
+
         Arrays.sort(nums);
 
         List<Integer> ans = new ArrayList<>();
@@ -38,11 +42,13 @@ public class FindAllDuplicates {
     }
 
     // Time - O (n), Space - O (n)
-    private static List<Integer> findDuplicates3(int[] nums) {
+    private static List<Integer> findDuplicates3(int[] numsOg) {
+        int[] nums = Arrays.copyOf(numsOg, numsOg.length);
+
         List<Integer> ans = new ArrayList<>();
         List<Integer> seen = new ArrayList<>();
 
-        for (int num: nums) {
+        for (int num : nums) {
             if (seen.contains(num)) {
                 ans.add(num);
             } else {
@@ -59,8 +65,10 @@ public class FindAllDuplicates {
         nums[idx2] = temp;
     }
 
-    // Time O(n) Space O(n)
-    private static List<Integer> findDuplicates4(int[] nums) {
+    // Time O(n) Modification of input array so Space O(n). Aux space - O(1)
+    private static List<Integer> findDuplicates4(int[] numsOg) {
+        int[] nums = Arrays.copyOf(numsOg, numsOg.length);
+
         int n = nums.length;
 
         int i = 0;
@@ -75,31 +83,47 @@ public class FindAllDuplicates {
 
         List<Integer> duplicates = new ArrayList<>();
         for (i = 0; i < n; i++) {
-            if (nums[i] != i+1) {
+            if (nums[i] != i + 1) {
                 duplicates.add(nums[i]);
             }
         }
         return duplicates;
     }
 
+    // Time O(n) Modification of input array so Space O(n). Aux space - O(1)
+    private static List<Integer> findDuplicates5(int[] nums) {
+        List<Integer> duplicates = new ArrayList<>();
+
+        for (int num : nums) {
+            if (nums[Math.abs(num) - 1] < 0) {
+                duplicates.add(Math.abs(num));
+            }
+            nums[Math.abs(num) - 1] *= -1;
+        }
+        return duplicates;
+    }
+
     public static void main(String[] args) {
-        int[] nums1 = {4,3,2,7,8,2,3,1};
+        int[] nums1 = {4, 3, 2, 7, 8, 2, 3, 1};
         System.out.println(findDuplicates1(nums1));
         System.out.println(findDuplicates2(nums1));
         System.out.println(findDuplicates3(nums1));
         System.out.println(findDuplicates4(nums1));
+        System.out.println(findDuplicates5(nums1));
 
-        int[] nums2 = {1,1,2};
+        int[] nums2 = {1, 1, 2};
         System.out.println(findDuplicates1(nums2));
         System.out.println(findDuplicates2(nums2));
         System.out.println(findDuplicates3(nums2));
         System.out.println(findDuplicates4(nums2));
+        System.out.println(findDuplicates5(nums2));
 
         int[] nums3 = {1};
         System.out.println(findDuplicates1(nums3));
         System.out.println(findDuplicates2(nums3));
         System.out.println(findDuplicates3(nums3));
         System.out.println(findDuplicates4(nums3));
+        System.out.println(findDuplicates5(nums3));
 
 
     }
