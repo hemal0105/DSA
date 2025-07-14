32ff96698605b71c7c55d837048d18178b6fbf95
hemal0105 <hemal0105@gmail.com>
2025-07-14 22:53:18 +0530

    rotate image

--- src/main/java/org/hm/problemsolving/RotateImage.java
+++ src/main/java/org/hm/problemsolving/RotateImage.java
@@ -2,6 +2,12 @@ package org.hm.problemsolving;
 
 import java.util.Arrays;
 
+/**
+ * You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+ * <p>
+ * You have to rotate the image in-place, which means you have to modify the input 2D matrix directly.
+ * DO NOT allocate another 2D matrix and do the rotation.
+ */
 public class RotateImage {
 
     public static void rotate(int[][] matrix) {
