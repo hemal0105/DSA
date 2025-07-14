5eeffdfd0ea096c15269b2c6fc6c8b608bdac15f
hemal0105 <hemal0105@gmail.com>
2025-07-14 22:52:29 +0530

    rotate image

---
+++ src/main/java/org/hm/problemsolving/RotateImage.java
@@ -0,0 +1,29 @@
+package org.hm.problemsolving;
+
+import java.util.Arrays;
+
+public class RotateImage {
+
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < (n + 1) / 2; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        rotate(matrix1);
+        System.out.println(Arrays.deepToString(matrix1));
+
+        int[][] matrix2 = {{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+        rotate(matrix2);
+        System.out.println(Arrays.deepToString(matrix2));
+    }
+}
