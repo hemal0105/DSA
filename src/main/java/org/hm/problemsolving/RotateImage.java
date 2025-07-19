a8b8b9cf35a0086fe020ce7344826e140ca2cc17
hemal0105 <hemal0105@gmail.com>
2025-07-19 23:32:59 +0530

    rotate image

--- src/main/java/org/hm/problemsolving/RotateImage.java
+++ src/main/java/org/hm/problemsolving/RotateImage.java
@@ -24,11 +24,46 @@ public class RotateImage {
         System.out.println(Arrays.deepToString(matrix));
     }
 
-    public static void main(String[] args) {
-        int[][] matrix1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        rotate(matrix1);
+    public static void rotate2(int[][] matrix) {
+        transpose(matrix);
+        reflect(matrix);
+        System.out.println(Arrays.deepToString(matrix));
+    }
 
-        int[][] matrix2 = {{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
-        rotate(matrix2);
+    private static void transpose(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int tmp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = tmp;
+            }
+        }
+    }
+
+    private static void reflect(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = tmp;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[][] matrix1_1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        rotate(matrix1_1);
+
+        int[][] matrix1_2 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        rotate2(matrix1_2);
+
+        int[][] matrix2_1 = {{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+        rotate(matrix2_1);
+
+        int[][] matrix2_2 = {{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+        rotate2(matrix2_2);
     }
 }
